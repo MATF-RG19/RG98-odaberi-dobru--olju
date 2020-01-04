551ca680d6fd9ef0923cb8f14b00da8a83db7963
MilanaNovakovic <milananovakovic007@gmail.com>
2020-01-04 15:44:40 -0500

    promena slike

--- main.c
+++ main.c
@@ -148,7 +148,7 @@ int main(int argc, char** argv){
     // kreira se prozor
     glutInitWindowSize(700, 500);
     glutInitWindowPosition(300, 300);
-    glutCreateWindow("Sibicarenje");
+    glutCreateWindow("Izaberi pravu solju");
     
     // registrovanje callback funkcija
     glutDisplayFunc(on_display);
